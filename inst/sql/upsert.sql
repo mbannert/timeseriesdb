fbd9027b39a8f9805c36435a0c2964c74dfdbea7
Matt Bannert <mbannert@users.noreply.github.com>
2014-11-18 09:36:59 +0100

    Update upsert.sql
    Extended upsert functionality to other timeseriesdb tables

--- inst/sql/upsert.sql
+++ inst/sql/upsert.sql
@@ -2,28 +2,64 @@
 # moving this update / insert operations to the 
 # database level
 
-create or replace function upsert_timeseries()
+create or replace function upsert_timeseries_tables()
 returns trigger
 as $$
 begin
-  if TG_OP = 'INSERT' then
-    if not exists (select 1 from timeseries_main
-    where ts_key = new.ts_key) then
-      return new;
-    else
-      update timeseries_main
-      set ts_data = new.ts_data,
+  case TG_TABLE_NAME
+    when 'timeseries_main' then
+      if TG_OP = 'INSERT' then
+        if not exists (select 1 from timeseries_main
+        where ts_key = new.ts_key) then
+          return new;
+        else
+          update timeseries_main
+          set ts_data = new.ts_data,
           ts_frequency = new.ts_frequency,
           md_generated_on = new.md_generated_on,
           md_generated_by = new.md_generated_by
-      where ts_key = new.ts_key;
-      return null;
-    end if;
-  end if;
+          where ts_key = new.ts_key;
+          return null;
+        end if;
+      end if;
+    when 'meta_data_localized' then
+      if TG_OP = 'INSERT' then
+        if not exists (select 1 from meta_data_localized
+        where ts_key = new.ts_key
+        and locale_info = new.locale_info) then
+          return new;
+        else
+          update meta_data_localized
+          set meta_data = new.meta_data
+          where ts_key = new.ts_key
+          and locale_info = new.locale_info;
+          return null;
+        end if;
+      end if;
+    when 'meta_data_unlocalized' then
+      if TG_OP = 'INSERT' then
+        if not exists (select 1 from meta_data_unlocalized
+        where ts_key = new.ts_key) then
+          return new;
+        else
+          update meta_data_unlocalized
+          set meta_data = new.meta_data
+          where ts_key = new.ts_key;
+          return null;
+        end if;
+      end if;
+  end case;
 end;
 $$ language plpgsql;
 
-create trigger upsert_timeseries
-  before insert on timeseries_main
-  for each row execute procedure upsert_timeseries();
+create trigger upsert_timeseries_tables
+before insert on timeseries_main
+for each row execute procedure upsert_timeseries_tables();
 
+create trigger upsert_timeseries_tables
+before insert on meta_data_localized
+for each row execute procedure upsert_timeseries_tables();
+
+create trigger upsert_timeseries_tables
+before insert on meta_data_unlocalized
+for each row execute procedure upsert_timeseries_tables();
