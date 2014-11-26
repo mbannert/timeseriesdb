3631a27cf6ac530128c3ba69107548252d05de36
Matthias Bannert <matthias.bannert@gmail.com>
2014-11-26 14:21:34 +0100

    added a delete function for time series and fixed Notes and warnings

--- R/getMeta.R
+++ R/getMeta.R
@@ -1,19 +1,18 @@
-#' Quick Handle operators
+#' Quickly Handle Meta Information
 #' 
 #' Sometimes reading the entire meta description for all language or
 #' multiple time series might not be necessary. Quick handle operators
 #' help users to access the information quickly as a non-nested list 
-#' for only one language is returned. These operators are alpha status, 
+#' for only one language is returned. These functions are alpha status, 
 #' more will follow. 
 #'
 #' 
 #' @param series an R time series object
-#' @param lang character representation of a language, typically 'en', 'de', 'fr' or 'it' as this is a Swiss package.
-#' @param con a Postgres Connection object, typically set in in options().
-#' @param tbl character name of the table that contains localized meta information
+#' @param lang character name of the language of the meta information. Typically 'de', 'it', 'fr' or 'en'.
+#' @param con connection object, defaults to object set by options().
+#' @param tbl character name of the table that contains the meta information. 
 #' @export
-#' @rdname quick_handle
-'%mdb%' <- function(series,lang,con = options()$TIMESERIESDB_CON,
+getMeta <- function(series, lang, con = options()$TIMESERIESDB_CON,
                     tbl = 'meta_data_localized'){
   if(!exists('con')) stop('No standard TIMESERIESDB_CON set. Quick handle operators need standard connection. Use options() to set TIMESERIESDB_CON.')
   ts <- deparse(substitute(series))
