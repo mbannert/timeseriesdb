7d01775824cabe067736fe280ebc675b07c63cc3
Matthias Bannert <matthias.bannert@gmail.com>
2015-01-26 19:36:20 +0100

    changed behavior of resolve overlap from error to warning when series have the same endpoint

--- R/resolveOverlap.R
+++ R/resolveOverlap.R
@@ -8,6 +8,20 @@
 #' @param ts1 ts time series, typically the older series
 #' @param ts2 ts time series, typically the younger series
 #' @export
+#' @examples
+#' ts1 <- ts(rnorm(100),start = c(1990,1),frequency = 4)
+#' ts2 <- ts(1:48,start = c(2000,1),frequency = 4)  
+#' resolveOverlap(ts1,ts2)   
+#' 
+#' # automatical detection of correction sequence!
+#' ts2 <- ts(rnorm(100),start = c(1990,1),frequency = 4)            
+#' ts1 <- ts(1:48,start = c(2000,1),frequency = 4)
+#' resolveOverlap(ts1,ts2)   
+#' 
+#' # both series are of the same length use sequence of arguments.
+#' ts1 <- ts(rnorm(100),start = c(1990,1),frequency = 4)
+#' ts2 <- ts(1:48,start = c(2003,1),frequency = 4)  
+#' resolveOverlap(ts1,ts2)
 resolveOverlap <- function(ts1,ts2){
   stopifnot(is.ts(ts1))
   stopifnot(is.ts(ts2))
@@ -20,8 +34,9 @@ resolveOverlap <- function(ts1,ts2){
     first <- ts2
     second <- ts1
   } else{
-    stop('Cannot resolve overlap automatically, both 
-         time series reach until the same period.')
+    warning('Cannot resolve overlap automatically, both time series reach until the same period. Using sequency of functions arguments.')
+    first <- ts1
+    second <- ts2
   }
   
   stopifnot(frequency(first) == frequency(second))
