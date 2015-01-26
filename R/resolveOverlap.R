572287dc0c3285e32797758ff3cb12fe0f70da9c
Matthias Bannert <matthias.bannert@gmail.com>
2015-01-26 23:59:37 +0100

    changed resolveOverlap behavior and included examples

--- R/resolveOverlap.R
+++ R/resolveOverlap.R
@@ -1,56 +1,72 @@
 #' Concatenate Time Series and Resolve Overlap Automatically
 #' 
-#' Resolve overlap determines which of two ts class time series is
+#' Append time series to each other. Resolve overlap determines
+#'  which of two ts class time series is
 #' reaching further and arranges the two series into first and second 
 #' series accordingly. Both time series are concatenated to one
-#' if both series had the same frequency. 
+#' if both series had the same frequency. Typically this function is used 
+#' concatenate two series that have a certain overlap, but one series clearly 
+#' starts earlier while the other lasts longer. If one series starts earlier and 
+#' stops later, all elements of the shorter series will be inserted into the 
+#' larger series, i.e. elements of the smaller series will replace the elements
+#' of the longer series.
 #'
 #' @param ts1 ts time series, typically the older series
 #' @param ts2 ts time series, typically the younger series
 #' @export
 #' @examples
 #' ts1 <- ts(rnorm(100),start = c(1990,1),frequency = 4)
-#' ts2 <- ts(1:48,start = c(2000,1),frequency = 4)  
+#' ts2 <- ts(1:18,start = c(2000,1),frequency = 4)  
 #' resolveOverlap(ts1,ts2)   
 #' 
 #' # automatical detection of correction sequence!
-#' ts2 <- ts(rnorm(100),start = c(1990,1),frequency = 4)            
-#' ts1 <- ts(1:48,start = c(2000,1),frequency = 4)
+#' ts1 <- ts(rnorm(90),start = c(1990,1),frequency = 4)            
+#' ts2 <- ts(1:60,start = c(2000,1),frequency = 4)
 #' resolveOverlap(ts1,ts2)   
 #' 
 #' # both series are of the same length use sequence of arguments.
 #' ts1 <- ts(rnorm(100),start = c(1990,1),frequency = 4)
 #' ts2 <- ts(1:48,start = c(2003,1),frequency = 4)  
-#' resolveOverlap(ts1,ts2)
+#'resolveOverlap(ts1,ts2)
 resolveOverlap <- function(ts1,ts2){
   stopifnot(is.ts(ts1))
   stopifnot(is.ts(ts2))
-  # check which time series is supposed to be 
-  # first and second
-  if(max(time(ts2)) > max(time(ts1))){
-    first <- ts1
-    second <- ts2
-  } else if(max(time(ts1)) > max(time(ts2))){
-    first <- ts2
-    second <- ts1
-  } else{
-    warning('Cannot resolve overlap automatically, both time series reach until the same period. Using sequency of functions arguments.')
-    first <- ts1
-    second <- ts2
+  stopifnot(frequency(ts1) == frequency(ts2))
+  freq <- frequency(ts1)
+    
+  ts1s <- min(time(ts1))
+  ts1e <- max(time(ts1))
+  
+  ts2s <- min(time(ts2))
+  ts2e <- max(time(ts2))
+  
+  # Cases ####
+  if(ts1s < ts2s & ts1e < ts2e){
+    out <- c(ts1[1:(which(time(ts1) == ts2s)-1)],ts2)
+    out <- ts(out,start = ts1s,frequency = freq)
   }
   
-  stopifnot(frequency(first) == frequency(second))
-  # stop if there is no overlap
-  if(max(time(first)) < min(time(second))){
-    stop('no overlap in time series.')
+  if(ts1s < ts2s & ts1e >= ts2e){
+    a <- which(time(ts1) == ts2s)
+    b <- which(time(ts1) == ts2e)
+    ts1[a:b] <- ts2
+    out <- ts1
   }
-  # check overlap
-  min_second <- min(time(second))
-  pos_first <- which(time(first) == min_second)
-  first_chunk <- first[1:(pos_first-1)]
-  ts(c(first_chunk,second),
-     frequency = frequency(first),
-     start = c(1990,1))
+
+  if(ts1s > ts2s & ts1e < ts2e){
+    a <- which(time(ts2) == ts1s)
+    b <- which(time(ts2) == ts1e)
+    ts2[a:b] <- ts1
+    out <- ts2
+  }
+  
+  if(ts1s > ts2s & ts1e > ts2e){
+    out <- c(ts2[1:(which(time(ts2) == ts1s)-1)],ts1)
+    out <- ts(out,start = ts2s,frequency = freq)
+  }
+  
+  out
+  
   
 }
 
