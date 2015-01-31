4ad3aa4dc87bb4d8c8992d871dc5c01964d84fc2
Matthias Bannert <matthias.bannert@gmail.com>
2015-01-31 13:30:31 +0100

    added optional WHERE clause elements to searchHstore

--- R/searchHstore.R
+++ R/searchHstore.R
@@ -1,4 +1,4 @@
-#' Search Key-Value Pairs
+#' Search Key-Value Pairs, look for existing keys in an Hstore
 #' 
 #' Search hstore key value in PostgreSQL. Very handsome when crawling the database by meta informaiton. 
 #' Currently works for non translated meta information.
@@ -8,12 +8,21 @@
 #' @param con PostgreSQL connection object
 #' @param hstore name of the hstore column
 #' @param tbl name of the table to be queried. defaults to  'meta_data_localized'
+#' @param where character restrict the SQL query by an additional where clause. Defaults to NULL.
+#' E.g.: ts_key LIKE '%ch%'.
 #' @rdname searchHstore
 #' @export
 searchKVP <- function(key,value,con = options()$TIMESERIESDB_CON,
-                      hstore = 'meta_data',tbl = 'meta_data_unlocalized'){
+                      hstore = 'meta_data',tbl = 'meta_data_unlocalized',
+                      where = NULL){
   if(is.null(con)) stop('Default TIMESERIESDB_CON not set in options() or no proper connection given to the con argument.')
-  sql_query <- sprintf("SELECT ts_key FROM %s WHERE %s->'%s'='%s'",tbl,hstore,key,value)
+  
+  # optional AND 
+  # Emulate sql's coalesce for dummies here 
+  # cause sprintf doesn't like NULL either
+  if(!is.null(where)) and <- paste0(" AND ",where)
+  
+  sql_query <- sprintf("SELECT ts_key FROM %s WHERE %s->'%s'='%s'%s",tbl,hstore,key,value)
   result <- dbGetQuery(con,sql_query)
   result$ts_key
 }
@@ -21,9 +30,17 @@ searchKVP <- function(key,value,con = options()$TIMESERIESDB_CON,
 
 #' @export
 lookForKey <- function(key,con = options()$TIMESERIESDB_CON,
-                       hstore = 'meta_data',tbl = 'meta_data_unlocalized'){
+                       hstore = 'meta_data',tbl = 'meta_data_unlocalized',
+                       where = NULL){
   if(is.null(con)) stop('Default TIMESERIESDB_CON not set in options() or no proper connection given to the con argument.')
-  sql_query <- sprintf("SELECT ts_key FROM %s WHERE %s ? '%s'",tbl,hstore,key)
+  
+  # optional AND 
+  # Emulate sql's coalesce for dummies here 
+  # cause sprintf doesn't like NULL either
+  and <- ''
+  if(!is.null(where)) and <- paste0(" AND ",where)
+  
+  sql_query <- sprintf("SELECT ts_key FROM %s WHERE %s ? '%s'%s",tbl,hstore,key,where)
   result <- dbGetQuery(con,sql_query)
   result$ts_key
 }
