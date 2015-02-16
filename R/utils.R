9848ff99ea3dd51b6ba7f14e558c0025a42f273c
Matthias Bannert <matthias.bannert@gmail.com>
2015-02-16 18:05:04 +0100

    added DBI dependency, fixed depth name and introduced isValid function

--- R/utils.R
+++ R/utils.R
@@ -8,5 +8,29 @@
 #' @details Hat tip to flodel at stackoverflow for suggesting this light weight way analyze depth of a nested list. Further complexity needs to be added to cover the fact that data.frame are lists, too. A more sophisticated recursive function can be found in the gatveys2 package.
 #' @references http://stackoverflow.com/questions/13432863/determine-level-of-nesting-in-r
 #' @export
-depth <- function(this) ifelse(is.list(this), 1L + max(sapply(this, depth)), 0L)
+getListDepth <- function(this) ifelse(is.list(this), 1L + max(sapply(this, depth)), 0L)
+
+
+
+
+
+#' Check whether an PostgreSQL object is valid or not.
+#' 
+#' Support function that verifies that the holding a reference to a
+#' foreign object is still valid for communicating with the RDBMS
+#' 
+#' @param dbObj a PostgreSQLConnection connection object. Check if the connection has expired.
+#' @return A logical scalar.
+#' @name dbIsValid
+NULL
+
+
+#' @rdname dbIsValid 
+#' @export
+setMethod("dbIsValid", "PostgreSQLConnection", function(dbObj) {
+  isValid <- tryCatch({dbGetInfo(dbObj)},
+                      error = function(e) NULL)
+  !is.null(isValid)  
+})
+
 
