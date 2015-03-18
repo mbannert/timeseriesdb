25da156ff84c16f2cf28e48d66f8f4aeb4ccc52b
Matthias Bannert <matthias.bannert@gmail.com>
2015-03-18 14:58:06 +0100

    added schema support to quick handle operators

--- R/quickHandleOps.R
+++ R/quickHandleOps.R
@@ -7,11 +7,11 @@
 #' @param regexp character regular expression pattern. 
 #' @rdname quickHandleOps
 #' @export
-"%k%" <- function(conObj,regexp){
+"%k%" <- function(conObj,regexp,schema = Sys.getenv("TIMESERIESDB_SCHEMA")){
   
   # get time series keys that suit the regexp
-  sql_query <- sprintf("SELECT ts_key FROM timeseries_main WHERE ts_key ~ '%s'",
-                       regexp)
+  sql_query <- sprintf("SELECT ts_key FROM %s.timeseries_main WHERE ts_key ~ '%s'",
+                       schema,regexp)
   keys <- dbGetQuery(conObj,sql_query)$ts_key
   
   ts_list <- readTimeSeries(keys,conObj)
@@ -28,12 +28,12 @@
 #' @param key character name of the key inside the hstore. 
 #' @export
 #' @rdname quickHandleOps
-createMetaDataHandle <- function(key){
+createMetaDataHandle <- function(key,schema = "timeseries"){
   
   sql_query <- sprintf("SELECT ts_key,
                         meta_data->'%s' AS %s
-                        FROM meta_data_unlocalized WHERE meta_data->'%s' ~ '",
-                        key,key,key)
+                        FROM %s.meta_data_unlocalized WHERE meta_data->'%s' ~ '",
+                        key,key,schema,key)
   
   sql_query <- paste0(sql_query,"%s'")
 
