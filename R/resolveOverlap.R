6f3179e98e3a740ccc8dfc504d7dac37462604ee
Matthias Bannert <matthias.bannert@gmail.com>
2015-06-04 20:03:45 +0200

    solved a problem in resolveOverlap caused by comparison of precision indices

--- R/resolveOverlap.R
+++ R/resolveOverlap.R
@@ -14,6 +14,7 @@
 #' @param ts1 ts time series, typically the older series
 #' @param ts2 ts time series, typically the younger series
 #' @param keep_ts2 logical should ts2 be kept? Defaults to TRUE.
+#' @param tolerance numeric when comparing min and max values with a index vector of a time series R runs in to trouble with precision handling, thus a tolerance needs to be set. Typically this does not need to be adjusted. E.g. 2010 != 2010.000. With the help of the tolerance parameter these two are equal.
 #' @export
 #' @examples
 #' ts1 <- ts(rnorm(100),start = c(1990,1),frequency = 4)
@@ -36,46 +37,49 @@
 #' ts1 <- ts(rnorm(50),start = c(1990,1),frequency = 4)            
 #' ts2 <- ts(1:100,start = c(1990,1),frequency = 4)
 #' resolveOverlap(ts1,ts2)
-resolveOverlap <- function(ts1,ts2,keep_ts2=T){
+resolveOverlap <- function (ts1, ts2, keep_ts2 = T, tolerance = 1e-3) 
+{
+  
+  # R seems to have some issues with comparing min time 
+  # because of double tolerance. got to set a tolerance here
+  tol <- tolerance
+  
+  
   stopifnot(is.ts(ts1))
   stopifnot(is.ts(ts2))
   stopifnot(frequency(ts1) == frequency(ts2))
   freq <- frequency(ts1)
-    
   ts1s <- min(time(ts1))
   ts1e <- max(time(ts1))
-  
   ts2s <- min(time(ts2))
   ts2e <- max(time(ts2))
-  
-  # Cases ####
-  if(ts1s < ts2s & ts1e < ts2e){
-    out <- c(ts1[1:(which(time(ts1) == ts2s)-1)],ts2)
-    out <- ts(out,start = ts1s,frequency = freq)
-  } else if(ts1s < ts2s & ts1e >= ts2e){
-    a <- which(time(ts1) == ts2s)
-    b <- which(time(ts1) == ts2e)
+  if (ts1s < ts2s & ts1e < ts2e) {
+    out <- c(ts1[1:(which(abs(time(ts1)-ts2s) <= tol) - 1)], ts2)
+    out <- ts(out, start = ts1s, frequency = freq)
+  }
+  else if (ts1s < ts2s & ts1e >= ts2e) {
+    a <- which(abs(time(ts1)-ts2s) <= tol)
+    b <- which(abs(time(ts1)-ts2e) <= tol)
     ts1[a:b] <- ts2
     out <- ts1
-  } else if(ts1s == ts2s & ts2e >= ts1e){
-    out <- ts2
-  } else if(ts1s > ts2s & ts1e < ts2e){
-    a <- which(time(ts2) == ts1s)
-    b <- which(time(ts2) == ts1e)
-    if(!keep_ts2){
-      ts2[a:b] <- ts1  
-    } 
-    out <- ts2
-  } else if(ts1s > ts2s & ts1e > ts2e){
-    out <- c(ts2[1:(which(time(ts2) == ts1s)-1)],ts1)
-    out <- ts(out,start = ts2s,frequency = freq)
-  } else {
-    stop('Case not covered, try switching ts1, ts2.')
   }
-  
-  
+  else if (ts1s == ts2s & ts2e >= ts1e) {
+    out <- ts2
+  }
+  else if (ts1s > ts2s & ts1e < ts2e) {
+    a <- which(abs(time(ts2)-ts1s) <= tol) 
+    b <- which(abs(time(ts2)-ts1s) <= tol) 
+    if (!keep_ts2) {
+      ts2[a:b] <- ts1
+    }
+    out <- ts2
+  }
+  else if (ts1s > ts2s & ts1e > ts2e) {
+    out <- c(ts2[1:(which(abs(time(ts2)-ts1s) <= tol) - 1)], ts1)
+    out <- ts(out, start = ts2s, frequency = freq)
+  }
+  else {
+    stop("Case not covered, try switching ts1, ts2.")
+  }
   out
-  
-  
 }
-
