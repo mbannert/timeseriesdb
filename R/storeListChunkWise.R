8b6c7ac8a20b9d15c41ce7977226f93ae0d74b40
Matthias Bannert <matthias.bannert@gmail.com>
2015-08-14 12:04:36 +0200

    added wrapper to avoid memory problems when storing large lists, fixed minor issues with case sensitive file names

---
+++ R/storeListChunkWise.R
@@ -0,0 +1,36 @@
+#' Store a List of Time Series Chunk Wise to Avoid Memory Problem
+#' 
+#' This function is a wrapper around \code{\link{storeTimeSeries}}. It is used to split large lists of time series
+#' according to memory limitations. This function uses INSERT INTO instead of the more convenient dbWritetable for performance reasons. DO NOT USE THIS FUNCTIONS IN LOOPS OR LAPPLY! This function can handle a set of time series on its own and is much faster than looping over a list. Non-unique primary keys are overwritten !
+#' 
+#' @author Matthias Bannert, Gabriel Bucur
+#' @param series character name of a time series, S3 class ts. When used with lists it is convenient to set series to names(li). Note that the series name needs to be unique in the database!
+#' @param con a PostgreSQL connection object.
+#' @param li list of time series. Defaults to NULL to no break legacy calls that use lookup environments.
+#' @param tbl character string denoting the name of the main time series table in the PostgreSQL database.
+#' @param md_unlocal character string denoting the name of the table that holds unlocalized meta information.
+#' @param lookup_env environment to look in for timeseries. Defaults to .GobalEnv.
+#' @param overwrite logical should existing records (same primary key) be overwritten? Defaults to TRUE.
+#' @param schema SQL schema name. Defaults to timeseries. 
+#' @importFrom DBI dbGetQuery
+#' @export
+storeListChunkWise <- function(series,
+                               con,
+                               li = NULL,
+                               tbl="timeseries_main",
+                               md_unlocal = "meta_data_unlocalized",
+                               lookup_env = .GlobalEnv,
+                               overwrite = T,
+                               schema = "timeseries"){
+  chunks <- ceiling(as.numeric(object.size(li))/(Cstack_info()["size"]*0.7))
+  name_chunks <- split(series,ceiling(seq_along(names(li))/chunks))
+  
+  # loop over the chunks in order to store it chunk wise 
+  # otherwise we run into stack limit on the server
+  for(i in seq_along(name_chunks)){
+    storeTimeSeries(name_chunks[[i]],con,li = li[name_chunks[[i]]])  
+  }
+}
+  
+  
+  
