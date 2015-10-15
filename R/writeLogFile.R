178df08638591ff82b3e380fbae251fdd5d163dc
Matthias Bannert <matthias.bannert@gmail.com>
2015-10-15 20:29:19 +0200

    parameter change

--- R/writeLogFile.R
+++ R/writeLogFile.R
@@ -3,11 +3,11 @@
 #' Most simple log file writer just write steps of
 #' a script to a text file. 
 #'
-#' @param filename character name of a textfile. Defaults to NULL.
 #' @param msg log file message
+#' @param filename character name of a textfile. Defaults to NULL.
 #' @param line_end line end character 
 #' @export
-writeLogFile <- function(filename=NULL,msg,line_end = "\n"){
+writeLogFile <- function(msg,filename=NULL,line_end = "\n"){
     if(is.null(filename)){
         filename <- paste0(Sys.Date(),".log")
     }
