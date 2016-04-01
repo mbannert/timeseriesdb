6c2effa24081e0d86eba2162af5f4ec61afb58fb
Matthias Bannert <matthias.bannert@gmail.com>
2016-04-01 12:15:40 +0200

    added an option the exportTsList to put time dimension on x-axis of a spreadsheet

--- R/exportTsList.R
+++ R/exportTsList.R
@@ -10,6 +10,8 @@
 #' @param dec character that separates decimals in .csv
 #' @param LC_TIME_LOCALE character time locale that differs from the standard locale. e.g. en_US.UTF-8. Defaults to NULL and uses the standard locale then. 
 #' @param date_format character denotes the date format. Defaults to NULL. If set to null the default is used: Jan 2010. In combination with LC\_TIME\_Locale various international date formats can be produced. 
+#' @param timeAsX logical should time be put to the x-axis of the spreadsheet?
+#' Defaults to FALSE.
 #' @importFrom reshape2 dcast
 #' @examples 
 #' tslist <- list()
@@ -28,17 +30,26 @@
 exportTsList <- function(tl,fname = NULL,cast = T, xlsx = F,
                          sep = ";",dec=".",
                          LC_TIME_LOCALE = NULL,
-                         date_format = NULL){
- 
+                         date_format = NULL,
+                         timeAsX = FALSE){
+  
+  # set file name chunk for the export file
+  if(is.null(fname)){
+    fname <- "timeseriesdb_export"
+  } 
+  fname <- paste0(fname,"_",gsub("-","_",Sys.Date()))
+  
   # check if all series got some frequencies 
-  # other we can't export
-  
+  # other we can't export, maybe we don't even need this anymore... 
+  # double check with next revision... 
   frq <- unique(sapply(tl,frequency))
-  
   if(length(frq) != 1) stop("All time series of within a list need to have the same frequency for proper export.")
-  
-   
-  tl <- lapply(tl,as.xts)
+
+  tl <- lapply(tl,function(x) {
+    xtsout <- xts::as.xts(x)
+    names(xtsout) <- "value"
+    xtsout
+    })
   out_list <- lapply(names(tl),function(x){
     dframe <- data.frame(time = time(tl[[x]]),
                          value = tl[[x]],row.names = NULL)
@@ -47,16 +58,10 @@ exportTsList <- function(tl,fname = NULL,cast = T, xlsx = F,
   })
   
   tsdf <- do.call("rbind",out_list)
-  if(is.null(fname)){
-    fname <- "timeseriesdb_export_"
-  } 
-  fname <- paste0(fname,"_",gsub("-","_",Sys.Date()))
-  #write.csv2(tsdf,file = fname,row.names = F)
-  #  reshape2::dcast(tsdf)
-  if(cast){
-    tsdf <- reshape2::dcast(tsdf,time ~ series)
-  }
+
   
+  # re format Date now before reshaping... 
+  # cause reshaping uses dates in col names etc. 
   # format the date
   if(!is.null(date_format)){
     PREV_LC_TIME_LOCALE <- Sys.getlocale("LC_TIME")
@@ -74,8 +79,31 @@ exportTsList <- function(tl,fname = NULL,cast = T, xlsx = F,
     } else {
       stop("need to provide a date_format argument if frequency is not monthly")
     }
+  }
+  
+  
+  # Now shape the data... 
+  # want to have time on the X axis??
+  if(timeAsX){
+    # time on the X axis
+    tsdf <- reshape(tsdf,timevar = "time",
+                    idvar = "series",
+                    direction="wide")
+    names(tsdf) <- gsub("value.","",names(tsdf))
     
-    
+    if(xlsx){
+      openxlsx::write.xlsx(tsdf,paste0(fname,".xlsx"))
+    } else{
+      write.table(tsdf,file = paste0(fname,".csv"), row.names = F, dec = dec, sep = sep)  
+    }
+  } else {
+    if(cast){
+      tsdf <- reshape(tsdf,
+                      idvar="time",
+                      timevar = "series",
+                      direction="wide")
+      names(tsdf) <- gsub("value.","",names(tsdf))
+    }  
   }
   
   if(xlsx){
