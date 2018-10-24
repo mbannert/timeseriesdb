46f7b8dd21579ad627ee675ff894dd12907ce82f
thoenis <sthoeni@kof.ethz.ch>
2018-10-24 09:08:43 +0200

    Add Print methods for tsmeta objects
    final implementation awaitng the hivemind

--- R/print_methods.R
+++ R/print_methods.R
@@ -8,3 +8,28 @@ print.meta_env <- function(x,...){
   
 }
 
+#' @export
+print.tsmeta.dt <- function(x) {
+  message("thems a tsmeta.dt!")
+  class(x) <- class(x)[2:length(class(x))]
+  print(x)
+}
+
+#' @export
+print.tsmeta.list <- function(x) {
+  message("smells like a tsmeta.list")
+  class(x) <- "list"
+  print(x)
+}
+
+#' @export
+print.tsmeta <- function(x) {
+  nam <- names(x)
+  for(i in seq_along(x)) {
+    cat(nam[i])
+    cat("\n")
+    cat(x[[i]])
+    cat("\n\n")
+  }
+  NULL
+}
