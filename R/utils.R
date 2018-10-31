2b2c6222696c107f857c8c164acd1474147eef5b
thoenis <sthoeni@kof.ethz.ch>
2018-10-31 13:13:16 +0100

    Migrate getListDepth

--- R/utils.R
+++ R/utils.R
@@ -45,6 +45,20 @@ setMethod("dbIsValid", "PostgreSQLConnection", function(dbObj) {
   !is.null(isValid)  
 })
 
+# recursive function to check depth of list. hat tip flodel 
+# at stackoverflow: http://stackoverflow.com/questions/13432863/determine-level-of-nesting-in-r
+#' Determine depth of a list
+#' 
+#' This function recursively checks the depth of a list and returns an integer value of depth
+#' 
+#' @param this an object of class list 
+#' @details Hat tip to flodel at stackoverflow for suggesting this light weight way analyze depth of a nested list. Further complexity needs to be added to cover the fact that data.frame are lists, too. A more sophisticated recursive function can be found in the gatveys2 package.
+#' @references http://stackoverflow.com/questions/13432863/determine-level-of-nesting-in-r
+#' @export
+getListDepth <- function(this) {
+  ifelse(is.list(this), 1L + max(sapply(this, getListDepth)), 0L)
+}
+
 stringSafeAsNumeric <- function(x) {
   y <- suppressWarnings(as.numeric(x))
   if(any(is.na(x) != is.na(y))) {
