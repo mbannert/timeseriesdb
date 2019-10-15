63f611229ff97adb0b771bac95ada0872d4b9963
thoenis <sthoeni@kof.ethz.ch>
2019-10-15 09:49:38 +0200

    add json_to_ts

---
+++ R/json_to_ts.R
@@ -0,0 +1,41 @@
+#' Convert json representation into time series
+#'
+#' @param jsn JSON string to convert
+#' @param as.dt Should the result be returned as a data.table?
+#' 
+#' @import data.table
+#' @importFrom xts xts
+#' @importFrom jsonlite fromJSON
+#' 
+#' @export
+json_to_ts <- function(jsn, as.dt = FALSE) {
+  dta <- fromJSON(jsn)
+  
+  if(as.dt) {
+    if(is.null(dta$frequency)) {
+      dta$frequency <- NA
+    }
+    
+    return(
+      as.data.table(dta)
+    )
+  }
+  
+  if(is.null(dta$frequency)) {
+    return(
+      xts(
+        dta$value,
+        order.by = as.Date(dta$time)
+      )
+    )
+  } else {
+    return(
+      ts(
+        dta$value,
+        start = date_to_index(dta$time[1]),
+        frequency = as.numeric(dta$frequency[1])
+      )
+      
+    )
+  }
+}
