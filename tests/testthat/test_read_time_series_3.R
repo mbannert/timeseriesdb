f48bba2df418a96322c593f6bd17e7c9a23db810
thoenis <sthoeni@kof.ethz.ch>
2019-10-24 14:19:25 +0200

    make tests more robust when db not up

--- tests/testthat/test_read_time_series_3.R
+++ tests/testthat/test_read_time_series_3.R
@@ -1,23 +1,22 @@
 context("read_time_series, case 3")
 
-con <- NULL
-if(is_test_db_reachable()) {
-  con <- connect_to_test_db()
-}
-
-reset_db(con)
-
 tsl_case_3 <- list(
   just_the_one = ts(rep(3, 10), 2019, frequency = 4)
 )
 class(tsl_case_3) <- c("tslist", "list")
 
-store_time_series(con, tsl_case_3, "ts for testing read case 3", "public")
+con <- NULL
+if(is_test_db_reachable()) {
+  con <- connect_to_test_db()
+  reset_db(con)
+  
+  store_time_series(con, tsl_case_3, "ts for testing read case 3", "public")
+}
 
 test_that("reading a simple ts works", {
   skip_on_cran()
   skip_if_not(is_test_db_reachable())
-  
+
   tsl_read <- read_time_series(con, "just_the_one")
   expect_equal(tsl_read, tsl_case_3)
 })
--- tests/testthat/test_read_time_series_4.R
+++ tests/testthat/test_read_time_series_4.R
@@ -1,12 +1,5 @@
 context("read_time_series, case 4")
 
-con <- NULL
-if(is_test_db_reachable()) {
-  con <- connect_to_test_db()
-}
-
-reset_db(con)
-
 tsl_state_1 <- list(
   ts1 = ts(rep(4, 10), 2019, frequency = 4)
 )
@@ -17,13 +10,19 @@ tsl_state_2 <- list(
 )
 class(tsl_state_2) <- c("tslist", "list")
 
-store_time_series(con, tsl_state_1, "ts for testing read case 4", "public", release_date = "1900-01-01")
-store_time_series(con, tsl_state_2, "second version of ts for testing read case 4", "public", release_date = Sys.Date() + 1)
+con <- NULL
+if(is_test_db_reachable()) {
+  con <- connect_to_test_db()
+  reset_db(con)
+  
+  store_time_series(con, tsl_state_1, "ts for testing read case 4", "public", release_date = "1900-01-01")
+  store_time_series(con, tsl_state_2, "second version of ts for testing read case 4", "public", release_date = Sys.Date() + 1)
+}
 
 test_that("it does not respect release date by default", {
   skip_on_cran()
   skip_if_not(is_test_db_reachable())
-  
+
   tsl_read <- read_time_series(con, "ts1")
   expect_equal(tsl_read, tsl_state_2)
 })
@@ -31,7 +30,7 @@ test_that("it does not respect release date by default", {
 test_that("it returns the proper version when respecting release date", {
   skip_on_cran()
   skip_if_not(is_test_db_reachable())
-  
+
   tsl_read <- read_time_series(con, "ts1", respect_release_date = TRUE)
   expect_equal(tsl_read, tsl_state_1)
 })
