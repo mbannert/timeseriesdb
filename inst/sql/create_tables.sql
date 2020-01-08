6316109600a78944e4c05ceae575ed5c7fa25bcb
thoenis <sthoeni@kof.ethz.ch>
2020-01-08 10:24:03 +0100

    touch up create_tables.sql

--- inst/sql/create_tables.sql
+++ inst/sql/create_tables.sql
@@ -1,87 +1,77 @@
-CREATE SCHEMA timeseries;
+BEGIN;
+-- make it easy to change the schema name:
+-- this is the only place it appears in the whole script
+CREATE SCHEMA IF NOT EXISTS timeseries;
+SET LOCAL search_path ='timeseries';
+
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS btree_gist;
 
-CREATE TABLE timeseries.timeseries_main(
+CREATE TABLE timeseries_main(
   id UUID NOT NULL DEFAULT uuid_generate_v1() PRIMARY KEY,
-  ts_key text, [ref: > series.ts_key]
-  validity daterange,
-  coverage daterange,
-  release_date timestamptz DEFAULT CURRENT_TIMESTAMP, -- users are expected to give release time plus tz by its name
-  ts_data json,
+  ts_key TEXT,
+  validity DATERANGE,
+  coverage DATERANGE,
+  release_date TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP, -- users are expected to give release time plus tz by its name
+  ts_data JSON,
   UNIQUE (ts_key, ts_validity),
-  FOREIGN KEY (ts_key) REFERENCES timeseries.timeseries_catalog(ts_key),
+  FOREIGN KEY (ts_key) REFERENCES timeseries_catalog(ts_key),
   EXCLUDE USING GIST (ts_key WITH =, validity WITH &&)
 )
 
 
-CREATE TABLE timeseries.timeseries_catalog(
-    ts_key text PRIMARY KEY,
-    set_id text,
-    created_by text,
-    created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+CREATE TABLE timeseries_catalog(
+    ts_key TEXT PRIMARY KEY,
+    set_id TEXT,
+    created_by TEXT,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (set_id) REFERENCES timeseries.datasets(set_id)
 )
 
-CREATE TABLE timeseries.datasets(
-    set_id text PRIMARY KEY,
-    set_md json
+CREATE TABLE datasets(
+    set_id TEXT PRIMARY KEY,
+    set_md JSON
 )
 
-CREATE TABLE timeseries.md_local_ts {
-  ts_key text,
-  lang text,
-  data_desc json,
+CREATE TABLE md_local_ts {
+  ts_key TEXT,
+  lang TEXT,
+  data_desc JSON,
   PRIMARY KEY(ts_key, lang),
   FOREIGN KEY (ts_key) REFERENCES timeseries.catalog(ts_key)
 }
 
 
-CREATE TABLE timeseries.md_local_vintages{
-  vintage_key text,
-  lang text, 
-  meta_data json,
+CREATE TABLE md_local_vintages{
+  vintage_key TEXT,
+  lang TEXT,
+  meta_data JSON,
   PRIMARY KEY(vintage_key, lang),
   FOREIGN KEY (vintage_key) REFERENCES timeseries.vintages(vintage_key)
 }
 
 
-CREATE TABLE timeseries.md_vintages{
-  vintage_key text,
-  meta_data json,
+CREATE TABLE md_vintages{
+  vintage_key TEXT,
+  meta_data JSON,
   PRIMARY KEY(vintage_key),
   FOREIGN KEY (vintage_key) REFERENCES timeseries.vintages(vintage_key)
 }
 
-CREATE TABLE timeseries.collections {
+CREATE TABLE collections {
   id UUID NOT NULL DEFAULT uuid_generate_v1() PRIMARY KEY,
-  name text,
-  owner text, 
-  description text,
+  name TEXT,
+  owner TEXT, 
+  description TEXT,
   UNIQUE (name, owner)
 }
 
-CREATE TABLE timeseries.collect_catalog {
+CREATE TABLE collect_catalog {
   id UUID, 
-  ts_key text, 
+  ts_key TEXT,
   PRIMARY KEY (id,ts_key),
   FOREIGN KEY (id) REFERENCES timeseries.collections(id),
   FOREIGN KEY (ts_key) REFERENCES timeseries.catalog(ts_key)
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+COMMIT;
