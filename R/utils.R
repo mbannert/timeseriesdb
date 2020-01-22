c0f24d390e5de92573f7befab219852639a7507f
thoenis <sthoeni@kof.ethz.ch>
2020-01-22 16:31:29 +0100

    invent db_call_function helper
    Because typing the same boilerplate for the 100th
    time got boring. And it's kind of clever...

--- R/utils.R
+++ R/utils.R
@@ -1,25 +1,25 @@
-#' 
+#'
 #' Helper function to convert time series indices of the form 2005.75
 #' to a date representation like 2005-07-01.
 #' Does not currently support sub-monthly frequencies.
-#' 
+#'
 #' @param x numeric A vector of time series time indices (e.g. from stats::time)
-#' @param as.string logical If as.string is TRUE the string representation of the 
+#' @param as.string logical If as.string is TRUE the string representation of the
 #' Date is returned, otherwise a Date object.
-#' 
+#'
 #' @export
-index_to_date <- function (x, as.string = FALSE) 
+index_to_date <- function (x, as.string = FALSE)
 {
   # If called as index_to_date(time(a_ts))
   # x is a ts. Unclass it so we can work with the faster basic operators
   x <- c(x)
-  
+
   years <- floor(x + 1/24)
   months <- floor(12*(x - years + 1/24)) + 1
   # No support for days currently
   # datestr <- paste(years, months, 1, sep = "-")
   datestr <- sprintf("%d-%02d-01", years, months)
-  
+
   if(!as.string) {
     return(as.Date(datestr))
   } else {
@@ -29,7 +29,7 @@ index_to_date <- function (x, as.string = FALSE)
 
 #' Convert date-likes to time index
 #'
-#' @param x The Date or Y-m-d string to convert 
+#' @param x The Date or Y-m-d string to convert
 #'
 #' @return The numeric representation of the date that can be used with ts
 #' @export
@@ -47,19 +47,19 @@ date_to_index <- function(x) {
   out
 }
 
-# recursive function to check depth of list. hat tip flodel 
+# recursive function to check depth of list. hat tip flodel
 # at stackoverflow: http://stackoverflow.com/questions/13432863/determine-level-of-nesting-in-r
 #' Determine depth of a list
-#' 
+#'
 #' This function recursively checks the depth of a list and returns an integer value of depth
-#' 
-#' @param this an object of class list 
+#'
+#' @param this an object of class list
 #' @details Hat tip to flodel at stackoverflow for suggesting this light weight way analyze depth of a nested list. Further complexity needs to be added to cover the fact that data.frame are lists, too. A more sophisticated recursive function can be found in the gatveys2 package.
 #' @references http://stackoverflow.com/questions/13432863/determine-level-of-nesting-in-r
 #' @export
 get_list_depth <- function(this) {
   ifelse(
-    is.list(this), 
+    is.list(this),
     ifelse(
       length(this) > 0,
       1L + max(sapply(this, get_list_depth)),
@@ -67,4 +67,33 @@ get_list_depth <- function(this) {
     ),
     0L
   )
+}
+
+
+#' Helper to construct SQL function calls
+#'
+#' Calls function `schema`.`fname` with the given `args`, returning
+#' the result.
+#'
+#' @param con PostgreSQL connection
+#' @param fname character Name of the function to be called
+#' @param schema character Name of the timeseries schema
+#' @param args unnamed list Function arguments
+#'
+#' @return value of `dbGetQuery(con, "SELECT * FROM schema.fname($args)")$fname`
+db_call_function <- function(con,
+                             fname,
+                             args = NULL,
+                             schema = "timeseries") {
+  query <- sprintf("SELECT * FROM %s.%s(%s)",
+                   dbQuoteIdentifier(con, schema),
+                   dbQuoteIdentifier(con, fname),
+                   ifelse(length(args > 0),
+                          paste(sprintf("$%d", 1:length(args)), collapse = ", "),
+                          ""))
+
+
+  res <- dbGetQuery(con, query, args)
+
+  res[[fname]]
 }
