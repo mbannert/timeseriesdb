bdcecc486e401e12d8191a757bc6cb459cba2b64
HomoCodens <32675029+HomoCodens@users.noreply.github.com>
2020-06-04 16:30:33 +0200

    fix setup_test_env

--- inst/sql/setup_test_env.sql
+++ inst/sql/setup_test_env.sql
@@ -1,19 +1,19 @@
 CREATE ROLE dev_reader_public;
-GRANT tsdb_access_public TO dev_reader_public;
-GRANT tsdb_reader TO dev_reader_public;
+GRANT timeseries_access_public TO dev_reader_public;
+GRANT timeseries_reader TO dev_reader_public;
 
 CREATE ROLE dev_writer_public;
-GRANT tsdb_access_public TO dev_writer_public;
-GRANT tsdb_writer TO dev_writer_public;
+GRANT timeseries_access_public TO dev_writer_public;
+GRANT timeseries_writer TO dev_writer_public;
 
-CREATE ROLE dev_reader_private;
-GRANT tsdb_access_private TO dev_reader_private;
-GRANT tsdb_reader TO dev_reader_private;
+CREATE ROLE dev_reader_main;
+GRANT timeseries_access_main TO dev_reader_main;
+GRANT timeseries_reader TO dev_reader_main;
 
-CREATE ROLE dev_writer_private;
-GRANT tsdb_access_private TO dev_writer_private;
-GRANT tsdb_writer TO dev_writer_private;
+CREATE ROLE dev_writer_main;
+GRANT timeseries_access_main TO dev_writer_main;
+GRANT timeseries_writer TO dev_writer_main;
 
 CREATE ROLE dev_admin WITH LOGIN; -- public/private distinction does not really make sense for admins
-GRANT tsdb_admin TO dev_admin;
+GRANT timeseries_admin TO dev_admin;
 GRANT CREATE, USAGE ON SCHEMA timeseries TO dev_admin;
