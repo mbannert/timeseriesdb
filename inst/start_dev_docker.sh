4353c81b8bb9cc4dab3592095a4b593dd4d9963c
Matt Bannert <matthias.bannert@gmail.com>
2020-06-07 08:04:56 +0200

    docker pg introduced a breaking change, not accepting no password configs anymore, accounting for this

--- inst/start_dev_docker.sh
+++ inst/start_dev_docker.sh
@@ -1,14 +1,14 @@
 docker stop timeseriesdb_dev
-docker run --rm -d -p 1111:5432 --name timeseriesdb_dev  postgres:11
-sleep 10
-psql -p 1111 -h 'localhost' -d postgres -U postgres -f sql/create_roles.sql
-psql -p 1111 -h 'localhost' -d postgres -U postgres -f sql/create_extensions.sql
-psql -p 1111 -h 'localhost' -d postgres -U postgres -f sql/setup_test_env.sql
-psql -p 1111 -h 'localhost' -d postgres -U dev_admin -f sql/create_tables.sql
-psql -p 1111 -h 'localhost' -d postgres -U dev_admin -f sql/create_functions_collections.sql
-psql -p 1111 -h 'localhost' -d postgres -U dev_admin -f sql/create_functions_datasets.sql
-psql -p 1111 -h 'localhost' -d postgres -U dev_admin -f sql/create_functions_metadata.sql
-psql -p 1111 -h 'localhost' -d postgres -U dev_admin -f sql/create_functions_ts.sql
-psql -p 1111 -h 'localhost' -d postgres -U dev_admin -f sql/create_triggers.sql
-psql -p 1111 -h 'localhost' -d postgres -U dev_admin -f sql/grant_rights.sql
+docker run --rm -d -p 1111:5432 --name timeseriesdb_dev -e POSTGRES_PASSWORD=pgking  postgres:11
+sleep 8
+PGPASSWORD=pgking psql -p 1111 -h 'localhost' -d postgres -U postgres -f sql/create_roles.sql
+PGPASSWORD=pgking psql -p 1111 -h 'localhost' -d postgres -U postgres -f sql/create_extensions.sql
+PGPASSWORD=pgking psql psql -p 1111 -h 'localhost' -d postgres -U postgres -f sql/setup_test_env.sql
+PGPASSWORD=dev_admin psql -p 1111 -h 'localhost' -d postgres -U dev_admin -f sql/create_tables.sql
+PGPASSWORD=dev_admin psql -p 1111 -h 'localhost' -d postgres -U dev_admin -f sql/create_functions_collections.sql
+PGPASSWORD=dev_admin psql -p 1111 -h 'localhost' -d postgres -U dev_admin -f sql/create_functions_datasets.sql
+PGPASSWORD=dev_admin psql -p 1111 -h 'localhost' -d postgres -U dev_admin -f sql/create_functions_metadata.sql
+PGPASSWORD=dev_admin psql -p 1111 -h 'localhost' -d postgres -U dev_admin -f sql/create_functions_ts.sql
+PGPASSWORD=dev_admin psql -p 1111 -h 'localhost' -d postgres -U dev_admin -f sql/create_triggers.sql
+PGPASSWORD=dev_admin psql -p 1111 -h 'localhost' -d postgres -U dev_admin -f sql/grant_rights.sql
 
