8bd851b44a797f01b99a64eefaf98ca34711a578
HomoCodens <32675029+HomoCodens@users.noreply.github.com>
2020-06-08 15:34:25 +0200

    remove extra psql from start_dev_docker.sh

--- inst/start_dev_docker.sh
+++ inst/start_dev_docker.sh
@@ -3,7 +3,7 @@ docker run --rm -d -p 1111:5432 --name timeseriesdb_dev -e POSTGRES_PASSWORD=pgk
 sleep 8
 PGPASSWORD=pgking psql -p 1111 -h 'localhost' -d postgres -U postgres -f sql/create_roles.sql
 PGPASSWORD=pgking psql -p 1111 -h 'localhost' -d postgres -U postgres -f sql/create_extensions.sql
-PGPASSWORD=pgking psql psql -p 1111 -h 'localhost' -d postgres -U postgres -f sql/setup_test_env.sql
+PGPASSWORD=pgking psql -p 1111 -h 'localhost' -d postgres -U postgres -f sql/setup_test_env.sql
 PGPASSWORD=dev_admin psql -p 1111 -h 'localhost' -d postgres -U dev_admin -f sql/create_tables.sql
 PGPASSWORD=dev_admin psql -p 1111 -h 'localhost' -d postgres -U dev_admin -f sql/create_functions_collections.sql
 PGPASSWORD=dev_admin psql -p 1111 -h 'localhost' -d postgres -U dev_admin -f sql/create_functions_datasets.sql
