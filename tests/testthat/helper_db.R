d5d085f106c63366d221caf22e09802e62700c94
HomoCodens <32675029+HomoCodens@users.noreply.github.com>
2020-06-08 17:03:26 +0200

    add password to connect test db function

--- tests/testthat/helper_db.R
+++ tests/testthat/helper_db.R
@@ -1,5 +1,5 @@
 connect_to_test_db <- function() {
-  dbConnect(Postgres(), "postgres", "localhost", 1111, "", "dev_admin", bigint = "integer")
+  dbConnect(Postgres(), "postgres", "localhost", 1111, "dev_admin", "dev_admin", bigint = "integer")
 }
 
 # TODO: see ?dbCanConnect
@@ -113,10 +113,10 @@ prepare_db <- function(con,
       '{"frequency": 12, "time": ["2020-01-01", "2020-02-01"], "value": [1, 2]}'
     ),
     access = c(
-      "public",
-      "public",
-      "main",
-      "main"
+      "timeseries_access_public",
+      "timeseries_access_public",
+      "timeseries_access_main",
+      "timeseries_access_main"
     )
   )
 
