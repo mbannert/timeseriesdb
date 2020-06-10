6299f115808531dd55a8d8965c57da4967a39d51
HomoCodens <32675029+HomoCodens@users.noreply.github.com>
2020-06-10 08:49:11 +0200

    add countdown to start_dev_docker

--- inst/start_dev_docker.sh
+++ inst/start_dev_docker.sh
@@ -1,6 +1,21 @@
 docker stop timeseriesdb_dev
 docker run --rm -d -p 1111:5432 --name timeseriesdb_dev -e POSTGRES_PASSWORD=pgking  postgres:12.3
-sleep 8
+echo '8'
+sleep 1
+echo '7'
+sleep 1
+echo '6'
+sleep 1
+echo '5'
+sleep 1
+echo '4'
+sleep 1
+echo '3'
+sleep 1
+echo '2'
+sleep 1
+echo '1'
+sleep 1
 PGPASSWORD=pgking psql -p 1111 -h 'localhost' -d postgres -U postgres -f sql/create_roles.sql
 PGPASSWORD=pgking psql -p 1111 -h 'localhost' -d postgres -U postgres -f sql/create_extensions.sql
 PGPASSWORD=pgking psql -p 1111 -h 'localhost' -d postgres -U postgres -f sql/setup_test_env.sql
