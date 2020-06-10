ce8d0e0800cc1b782d9e739e2a72478f1c6bc500
HomoCodens <32675029+HomoCodens@users.noreply.github.com>
2020-06-10 16:20:29 +0200

    fix error message for wrong access

--- R/store_records.R
+++ R/store_records.R
@@ -34,7 +34,7 @@ store_records <- function(con,
       if(grepl("permission denied for function insert_from_tmp", e)) {
         stop("Only writer and above may store time series.")
       } else if(grepl("timeseries_main_access_fkey", e)) {
-        stop(sprintf("\"%s\" is not a valid access level. Use db_get_access_levels to find registered levels.", schema))
+        stop(sprintf("\"%s\" is not a valid access level. Use db_get_access_levels to find registered levels.", access))
       } else {
         stop(e)
       }
