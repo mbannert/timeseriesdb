537e07ce2b3f3d01dda3fbe130a681a84c82a6a5
HomoCodens <32675029+HomoCodens@users.noreply.github.com>
2020-06-11 10:15:16 +0200

    fine tune gsub in setup_sql_functions

--- R/setup_sql.R
+++ R/setup_sql.R
@@ -130,7 +130,8 @@ setup_sql_functions <- function(con, schema = "timeseries"){
 
   for(f in fls) {
     sql <- readLines(f)
-    sql <- gsub("timeseries", schema, sql)
+    # [^m] to exclude the TABLE timeseries_main but include timeseries_admin
+    sql <- gsub("timeseries([.,_][^m])", sprintf("%s\\1", schema), sql)
     # split up SQL by a new set of lines everytime CREATE FUNCTION
     # occurs in order to send single statements using multiple execute calls
     # which is DBI / RPostgres compliant
