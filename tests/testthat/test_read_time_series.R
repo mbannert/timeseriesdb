e1e9648a2ceb2bdd354a2828db91785b6b6ace18
HomoCodens <32675029+HomoCodens@users.noreply.github.com>
2020-06-22 10:44:28 +0200

    move ts read fixture into helper_db

--- tests/testthat/test_read_time_series.R
+++ tests/testthat/test_read_time_series.R
@@ -2,29 +2,28 @@
 
 context("read_time_series")
 
-# TODO: move this into helper_db as a fixture
 tsl_state_0 <- list(
-  ts1 = ts(rep(1.9, 10), 2019, frequency = 4)
+  rts1 = ts(rep(1.9, 10), 2019, frequency = 4)
 )
 class(tsl_state_0) <- c("tslist", "list")
 
 tsl_state_1 <- list(
-  ts1 = ts(rep(2, 10), 2019, frequency = 4)
+  rts1 = ts(rep(2, 10), 2019, frequency = 4)
 )
 class(tsl_state_1) <- c("tslist", "list")
 
 tsl_state_2 <- list(
-  ts1 = ts(rep(2.1, 10), 2019, frequency = 4)
+  rts1 = ts(rep(2.1, 10), 2019, frequency = 4)
 )
 class(tsl_state_2) <- c("tslist", "list")
 
 tsl_state_2_v2 <- list(
-  ts1 = ts(rep(2.1415926, 10), 2019, frequency = 4)
+  rts1 = ts(rep(2.1415926, 10), 2019, frequency = 4)
 )
 class(tsl_state_2_v2) <- c("tslist", "list")
 
 tsl_pblc <- list(
-  tsp = ts(rep(3, 10), 2019, frequency = 4)
+  rtsp = ts(rep(3, 10), 2019, frequency = 4)
 )
 class(tsl_pblc) <- c("tslist", "list")
 
@@ -32,121 +31,80 @@ if(is_test_db_reachable()) {
   con_admin <- connect_to_test_db()
   con_reader_public <- connect_to_test_db("dev_reader_public")
   con_reader_main <- connect_to_test_db("dev_reader_main")
-
-  reset_db(con_admin)
-
-  current_date <- dbGetQuery(con_admin, "SELECT CURRENT_DATE")$current_date
-
-  # TODO: This would be more robust if we took charge of what time it is entirely (on db and here)
-  store_time_series(con_admin,
-                    tsl_state_0,
-                    "tsdb_test_access_main",
-                    valid_from = current_date - 4,
-                    release_date = current_date - 4,
-                    schema = "tsdb_test")
-  store_time_series(con_admin,
-                    tsl_state_1,
-                    "tsdb_test_access_main",
-                    valid_from = current_date - 3,
-                    release_date = current_date - 1,
-                    schema = "tsdb_test")
-  store_time_series(con_admin,
-                    tsl_state_2,
-                    "tsdb_test_access_main",
-                    valid_from = current_date - 1,
-                    release_date = current_date + 2,
-                    schema = "tsdb_test")
-  store_time_series(con_admin,
-                    tsl_state_2,
-                    "tsdb_test_access_main",
-                    valid_from = current_date - 1,
-                    release_date = current_date + 2,
-                    schema = "tsdb_test")
-  store_time_series(con_admin,
-                    tsl_state_2_v2,
-                    "tsdb_test_access_main",
-                    valid_from = current_date + 1,
-                    release_date = current_date + 2,
-                    schema = "tsdb_test")
-
-  store_time_series(con_admin,
-                    tsl_pblc,
-                    "tsdb_test_access_public",
-                    schema = "tsdb_test")
 }
 
-test_that("public reader may not read main series", {
+test_with_fresh_db(con_admin, "public reader may not read main series", {
   skip_on_cran()
   skip_if_not(is_test_db_reachable())
 
-  tsl_read <- read_time_series(con_reader_public, "ts1", schema = "tsdb_test")
+  tsl_read <- read_time_series(con_reader_public, "rts1", schema = "tsdb_test")
   expect_length(tsl_read, 0)
 })
 
-test_that("series with no access get skipped", {
+test_with_fresh_db(con_admin, "series with no access get skipped", {
   skip_on_cran()
   skip_if_not(is_test_db_reachable())
 
-  tsl_read <- read_time_series(con_reader_public, c("ts1", "tsp"), schema = "tsdb_test")
+  tsl_read <- read_time_series(con_reader_public, c("rts1", "rtsp"), schema = "tsdb_test")
   expect_equal(tsl_read, tsl_pblc)
 })
 
-test_that("by default it reads the most recent valid vintage", {
+test_with_fresh_db(con_admin, "by default it reads the most recent valid vintage", {
   skip_on_cran()
   skip_if_not(is_test_db_reachable())
 
-  tsl_read <- read_time_series(con_reader_main, "ts1", schema = "tsdb_test")
+  tsl_read <- read_time_series(con_reader_main, "rts1", schema = "tsdb_test")
   expect_equal(tsl_read, tsl_state_2)
 })
 
-test_that("by default it reads the most recent valid vintage but with respecting rls date", {
+test_with_fresh_db(con_admin, "by default it reads the most recent valid vintage but with respecting rls date", {
   skip_on_cran()
   skip_if_not(is_test_db_reachable())
 
   tsl_read <- read_time_series(con_reader_main,
-                               "ts1",
+                               "rts1",
                                respect_release_date = TRUE,
                                schema = "tsdb_test")
   expect_equal(tsl_read, tsl_state_1)
 })
 
-test_that("reading desired vintages works", {
+test_with_fresh_db(con_admin, "reading desired vintages works", {
   skip_on_cran()
   skip_if_not(is_test_db_reachable())
 
   tsl_read_1 <- read_time_series(con_reader_main,
-                                 "ts1",
-                                 valid_on = current_date - 4,
+                                 "rts1",
+                                 valid_on = Sys.Date() - 4,
                                  schema = "tsdb_test")
   expect_equal(tsl_read_1, tsl_state_0)
 
   tsl_read_2 <- read_time_series(con_reader_main,
-                                 "ts1",
-                                 valid_on = current_date - 2,
+                                 "rts1",
+                                 valid_on = Sys.Date() - 2,
                                  schema = "tsdb_test")
   expect_equal(tsl_read_2, tsl_state_1)
 })
 
-test_that("reading vintages, respecting release date", {
+test_with_fresh_db(con_admin, "reading vintages, respecting release date", {
   skip_on_cran()
   skip_if_not(is_test_db_reachable())
 
   tsl_read <- read_time_series(con_reader_main,
-                               "ts1",
-                               valid_on = current_date - 2,
+                               "rts1",
+                               valid_on = Sys.Date() - 2,
                                respect_release_date = TRUE,
                                schema = "tsdb_test")
   expect_equal(tsl_read, tsl_state_1)
 })
 
-test_that("reading via regex works", {
+test_with_fresh_db(con_admin, "reading via regex works", {
   skip_on_cran()
   skip_if_not(is_test_db_reachable())
 
   tsl_read <- read_time_series(con_reader_main,
-                               "^ts",
+                               "^rts",
                                regex = TRUE,
                                schema = "tsdb_test")
-  expect_setequal(names(tsl_read), c("ts1", "tsp"))
+  expect_setequal(names(tsl_read), c("rts1", "rtsp"))
 })
 
