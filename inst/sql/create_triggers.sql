f675b9573f05c6f384c00934b7bf8649d4b8768a
HomoCodens <32675029+HomoCodens@users.noreply.github.com>
2020-06-25 10:13:30 +0200

    add trigger to ensure access levels are roles

--- inst/sql/create_triggers.sql
+++ inst/sql/create_triggers.sql
@@ -17,3 +17,27 @@ BEFORE DELETE
 ON timeseries.datasets
 FOR EACH ROW
 EXECUTE PROCEDURE timeseries.prevent_delete_default_dataset();
+
+CREATE FUNCTION timeseries.ensure_access_level_is_role()
+RETURNS TRIGGER AS
+$$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1
+    FROM pg_catalog.pg_roles
+    WHERE rolname = NEW.role
+  ) THEN
+    RAISE EXCEPTION 'Role % does not exist so it can not be an access level.', NEW.role;
+  END IF;
+
+  RETURN NEW;
+END;
+$$ LANGUAGE PLPGSQL
+SECURITY DEFINER
+SET search_path = timeseries, pg_temp;
+
+CREATE TRIGGER access_level_role_check
+BEFORE INSERT OR UPDATE
+ON timeseries.access_levels
+FOR EACH ROW
+EXECUTE PROCEDURE timeseries.ensure_access_level_is_role();
