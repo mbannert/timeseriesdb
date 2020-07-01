c70a1e5392fcc44a674ccb27860b1dda11485f29
HomoCodens <32675029+HomoCodens@users.noreply.github.com>
2020-07-01 10:05:31 +0200

    add tests for change_access_levels

--- tests/testthat/test_access_levels.R
+++ tests/testthat/test_access_levels.R
@@ -15,6 +15,50 @@ test_with_fresh_db(con_admin, "reader may not change access levels", {
   )
 })
 
+test_with_fresh_db(con_admin, "setting access level to unregistered one", {
+  expect_error(
+    db_change_access_level(con_writer, "vts1", "fort_knox", schema = "tsdb_test"),
+    "fort_knox is not a valid"
+  )
+})
+
+test_with_fresh_db(con_admin, "setting access level return status", {
+  out <- db_change_access_level(con_writer, "vts1", "tsdb_test_access_public", schema = "tsdb_test")
+
+  expect_equal(out, list(status = "ok"))
+})
+
+test_with_fresh_db(con_admin, "setting access level for all vintages", {
+  db_change_access_level(con_writer, c("vts1", "vts2"), "tsdb_test_access_restricted", schema = "tsdb_test")
+
+  res <- dbGetQuery(con_admin, "SELECT * FROM tsdb_test.timeseries_main WHERE ts_key ~ 'vts' ORDER BY ts_key, validity")
+
+  expect_equal(
+    res$access,
+    rep("tsdb_test_access_restricted", 4)
+  )
+})
+
+test_with_fresh_db(con_admin, "setting access level for specific vintages", {
+  db_change_access_level(con_writer,
+                         c("vts1", "vts2"),
+                         "tsdb_test_access_restricted",
+                         validity = "2020-01-01",
+                         schema = "tsdb_test")
+
+  res <- dbGetQuery(con_admin, "SELECT * FROM tsdb_test.timeseries_main WHERE ts_key ~ 'vts' ORDER BY ts_key, validity")
+
+  expect_equal(
+    res$access,
+    c(
+      "tsdb_test_access_restricted",
+      "tsdb_test_access_public",
+      "tsdb_test_access_restricted",
+      "tsdb_test_access_main"
+    )
+  )
+})
+
 # test db_list_access_levels --------------------------------------------------
 test_with_fresh_db(con_admin, "db_list_access_levels returns data frame with correct names", {
   out <- db_list_access_levels(con_reader, schema = "tsdb_test")
