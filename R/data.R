005e30f6abf8bd50cfa14865cf98b4e3bc53904d
angelicambg <angelicambg@gmail.com>
2020-07-08 10:32:09 +0200

    demo data with fok series

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' KOF indicators
+#'
+#' @source KOF Swiss Economic Institute - KOF indicators.
+#' \url{https://kof.ethz.ch/en/forecasts-and-indicators/indicators.html}
+#' @format A list with four time series objects:
+#' \describe{
+#'  \item{kofbarometer}{Indicator for the Swiss Business Cycle. Montlhy serie from 2000 to 2018.}
+#'  \item{kofbsi}{Indicator based on surveys conducted to companies active in the industry, retail, construction, project engineering, as well as financial and insurance services. Montlhy serie from 2010 to 2018.}
+#'  \item{kofei}{The indicator is calculated on the basis of KOF Business Tendency Surveys. Quarterly serie from 2000 to 2018.}
+#'  \item{kofmpc}{xts object. It provides a quantitative measure of ECB communication. Irregular serie from 2000 to 2018.}
+#' }
+#' @examples
+#' \dontrun{
+#'  kof_ts
+#' }
+"kof_ts"
+
