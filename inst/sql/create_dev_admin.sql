1e341df0912977f35c08f4530eec50247f6b3713
mbannert <matthias.bannert@gmail.com>
2020-07-23 13:19:36 +0200

    static page only

--- inst/sql/create_dev_admin.sql
+++
@@ -1,3 +0,0 @@
-CREATE ROLE dev_admin WITH LOGIN PASSWORD 'dev_admin'; -- public/private distinction does not really make sense for admins
-GRANT timeseries_admin TO dev_admin;
-GRANT CREATE, USAGE ON SCHEMA timeseries TO dev_admin;
--- tests/README.txt
+++
@@ -1,3 +0,0 @@
-The following extensions need to be installed for the tests (and tsdb as a whole for that matter) to work:
-- hstore
-- btree_gist
