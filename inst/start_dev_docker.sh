515d1651f59a1fecaf4678a9e2b1c3f32bb6def0
HomoCodens <32675029+HomoCodens@users.noreply.github.com>
2020-08-12 10:08:06 +0200

    fix rights in setup scripts
    stemming from the fact that install_timeseriesdb now uses the
    admin group instead of a specific user

--- inst/start_dev_docker.sh
+++ inst/start_dev_docker.sh
@@ -19,12 +19,12 @@ sleep 1
 # I leave it as an excercise for future generations to get this to work on git bash under windows
 #  PGPASSWORD=pgking sed 's/timeseries/banaan/g' sql/create_roles.sql | psql -p 1111 -h 'localhost' -d postgres -U postgres
 
-sed 's/timeseries/tsdb_test/g' sql/create_roles.sql > roles.sql
-PGPASSWORD=pgking psql -p 1111 -h 'localhost' -d postgres -U postgres -f roles.sql
-rm roles.sql
 sed 's/timeseries/tsdb_test/g' sql/create_extensions.sql > extensions.sql
 PGPASSWORD=pgking psql -p 1111 -h 'localhost' -d postgres -U postgres -f extensions.sql
 rm extensions.sql
+sed 's/timeseries/tsdb_test/g' sql/create_roles.sql > roles.sql
+PGPASSWORD=pgking psql -p 1111 -h 'localhost' -d postgres -U postgres -f roles.sql
+rm roles.sql
 sed 's/timeseries/tsdb_test/g' sql/create_dev_admin.sql > admin.sql
 PGPASSWORD=pgking psql -p 1111 -h 'localhost' -d postgres -U postgres -f admin.sql
 rm admin.sql
