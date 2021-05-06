88e6080fe587e99b31a9f211ac1d6da00aa13b8f
HomoCodens <32675029+HomoCodens@users.noreply.github.com>
2021-05-06 15:59:36 +0200

    remove ts smell check from db_ts_store.list
    db_ts_store.tslist already does that
    
    closes #239

--- tests/testthat/test_store_time_series.R
+++ tests/testthat/test_store_time_series.R
@@ -6,6 +6,11 @@ tsl <- list(
 )
 class(tsl) <- c("tslist", "list")
 
+tsl_list <- tsl
+class(tsl_list) <- "list"
+
+
+
 dt <- data.table(
   id = rep(c("ts1", "ts2"), each = 2),
   time = seq(as.Date("2019-01-01"), length.out = 2, by = "1 month"),
@@ -16,7 +21,7 @@ ts <- tsl$ts1
 
 xts <- xts::as.xts(ts)
 
-# store time series from lists  ##########################
+# store time series from tslists  ##########################
 context("db_ts_store.tslist")
 
 test_that("it calls through to store_records", {
@@ -108,6 +113,106 @@ test_that("it handles non-ts-likes", {
 test_that("It handles duplicate names", {
   tsl_local <- tsl
   names(tsl_local) <- c("tsa", "tsa")
+  expect_error(
+    db_ts_store("con", tsl_local, "release", "access"),
+    "duplicate keys"
+  )
+})
+
+# store time series from lists  ##########################
+context("db_ts_store.list")
+
+test_that("it calls through to store_records", {
+  store_recs <- mock()
+  with_mock(
+    # https://github.com/r-lib/testthat/issues/734#issuecomment-377367516
+    "timeseriesdb:::store_records" = store_recs,
+    "timeseriesdb:::to_ts_json" = mock("ts_json"), # Oh how I love isolating units under test. You are going to die all alone, little function...
+                                                   # One in the middle of a pandemic later: Poor thing :'(
+    {
+      db_ts_store(
+        "con",
+        tsl_list,
+        "access",
+        "valid_from",
+        "release_date",
+        "pre_release_access",
+        "schema"
+      )
+
+      expect_called(store_recs, 1)
+
+      expect_args(
+        store_recs,
+        1,
+        "con",
+        "ts_json",
+        "access",
+        "timeseries_main",
+        "valid_from",
+        "release_date",
+        "pre_release_access",
+        "schema"
+      )
+    }
+  )
+})
+
+
+test_that("defaults are passed on correctly", {
+  store_recs <- mock()
+  with_mock(
+    "timeseriesdb:::store_records" = store_recs,
+    "timeseriesdb:::to_ts_json" = mock("ts_json"), # Oh how I love isolating units under test. You are going to die all alone, little function...
+    {
+      db_ts_store(
+        "con",
+        tsl_list
+      )
+
+      expect_called(store_recs, 1)
+
+      expect_args(
+        store_recs,
+        1,
+        "con",
+        "ts_json",
+        NULL,
+        "timeseries_main",
+        NULL,
+        NULL,
+        NULL,
+        "timeseries"
+      )
+    }
+  )
+})
+
+test_that("it handles empty lists", {
+  tsl <- list()
+  expect_warning(xx <- db_ts_store("con", tsl), "no-op")
+  expect_equal(xx, list())
+})
+
+test_that("it handles non-ts-likes", {
+  tsl_local <- tsl
+  tsl_local$not_a_ts <- "Mwahahaha!"
+  class(tsl_local) <- "list"
+  store_recs <- mock()
+  with_mock(
+    "timeseriesdb:::store_records"= store_recs,
+    {
+      expect_message(
+        db_ts_store("con", tsl_local, "release", "access"),
+        "no valid time series objects.*not_a_ts"
+      )
+    })
+})
+
+test_that("It handles duplicate names", {
+  tsl_local <- tsl
+  names(tsl_local) <- c("tsa", "tsa")
+  class(tsl_local) <- "list"
   expect_error(
     db_ts_store("con", tsl_local, "release", "access"),
     "duplicate keys"
